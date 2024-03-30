03837b2ab8522404617c46f6f3291af98a92a511
MikronT <vlad.ivanov.if@gmail.com>
2024-03-30 15:20:43 +0200

    Extend CodeHighlighter

---
+++ scenes/editor/highlighter/init.gd
@@ -0,0 +1,21 @@
+class_name DefaultCodeHighligher
+extends CodeHighlighter
+
+var keyword_color = Color("f92672")
+var comment_color = Color("75715e")
+var string_color = Color("e6db74")
+
+var keywords = ["func", "if", "else"]
+
+
+func _init():
+	number_color = Color("ae81ff")
+	symbol_color = Color("f8f8dc")
+	function_color = Color("a6e22d")
+	member_variable_color = function_color
+
+	for word in keywords:
+		add_keyword_color(word, keyword_color)
+
+	add_color_region("#", "", comment_color)
+	add_color_region('"', '"', string_color)
