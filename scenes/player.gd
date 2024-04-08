1e9c1b54a99f5b68b737c663ee58fb85862dde01
MikronT <vlad.ivanov.if@gmail.com>
2024-04-08 22:06:31 +0300

    Attach player script to handle movement

---
+++ scenes/player.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+@export var speed = 50
+
+@export var tile_size: int = 32
+var tile_center = Vector2(int(tile_size / 2), tile_size / 2)
+
+@onready var position_grid = coord_to_grid(position)
+@onready var target = position
+
+var active = false
+
+
+func _get_direction():
+	var direction = Vector2.ZERO
+
+	if active:
+		return direction
+
+	if Input.is_action_just_pressed("up"):
+		direction.y -= 1
+	if Input.is_action_just_pressed("down"):
+		direction.y += 1
+	if Input.is_action_just_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_just_pressed("right"):
+		direction.x += 1
+
+	return direction
+
+
+func _physics_process(_delta):
+	var direction = _get_direction()
+
+	if direction != Vector2.ZERO:
+		position_grid += direction
+		target = grid_to_coord(position_grid)
+		print("Moving from ", position, " to ", target)
+
+		velocity = position.direction_to(target) * speed
+
+	if position.distance_to(target) > 1:
+		active = true
+		move_and_slide()
+	else:
+		active = false
+
+
+func coord_to_grid(coord: Vector2):
+	return (coord - tile_center) / tile_size
+
+
+func grid_to_coord(grid: Vector2):
+	return grid * tile_size + tile_center
