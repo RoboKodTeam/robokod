85bfe19c7abfa9f4726b081f610384dbd6717707
MikronT <vlad.ivanov.if@gmail.com>
2024-04-13 21:59:21 +0300

    Add automatic level scaling

--- scenes/ide/ide_test.gd
+++ scenes/ide/ide_test.gd
@@ -6,5 +6,4 @@ extends Control
 func _ready():
 	var level = preload("res://scenes/levels/level1.tscn")
 	var instance = level.instantiate()
-	instance.apply_scale(Vector2(2, 2))
 	level_container.set_level(instance)
--- scenes/level_container.gd
+++ scenes/level_container.gd
@@ -9,22 +9,34 @@ func _get_maximum_content_size():
 	return max_size
 
 
-func _get_content_position():
+func _get_relative_content_scale():
+	var wanted_size = get_parent_area_size() * 0.85
 	var min_size = _get_maximum_content_size()
-	return (get_parent_area_size() - min_size) / 2
+
+	var scale_rect = Vector2(wanted_size.x / min_size.x, wanted_size.y / min_size.y)
+	var scale_factor = min(scale_rect.x, scale_rect.y)
+
+	return Vector2(scale_factor, scale_factor)
+
+
+func _get_content_position():
+	var available_size = get_parent_area_size()
+	var min_size = _get_maximum_content_size()
+	return (available_size - min_size) / 2
 
 
 func _notification(what):
 	if what == NOTIFICATION_SORT_CHILDREN:
-		var content_position = _get_content_position()
-
 		# Must re-sort the children
 		for child in get_children():
-			child.set_position(content_position)
+			var content_scale = _get_relative_content_scale()
+			child.scale *= content_scale
+
+			var content_position = _get_content_position()
+			child.position = content_position
 
 
 func _on_emulator_resized():
-	Log.info("Emulator resized, asking to redraw")
 	queue_sort()
 
 
@@ -32,4 +44,5 @@ func set_level(level: Node2D):
 	for child in get_children():
 		remove_child(child)
 
+	Log.info("Setting level")
 	add_child(level)
