2caa64ddc96bf995ba27e1f8a021556d2eb0071b
MikronT <vlad.ivanov.if@gmail.com>
2024-04-13 17:34:36 +0300

    Count TileMap and Level dimensions

---
+++ scenes/level.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var tile_map = $TileMap
+
+
+func get_level_dimensions():
+	var map_dimens = tile_map.get_map_dimensions()
+	var level_dimens = Vector2(map_dimens.x * scale.x, map_dimens.y * scale.y)
+	Log.prn("Level dimensions counted: ", level_dimens)
+	return level_dimens
