77214f78190a80cd22cc92f0512863efd2b48584
MikronT <vlad.ivanov.if@gmail.com>
2024-04-14 01:02:12 +0300

    Bind editor tab name to script

--- scenes/ide/ide_test.gd
+++ scenes/ide/ide_test.gd
@@ -1,9 +1,12 @@
 extends Control
 
 @onready var level_container = %LevelContainer
+@onready var level_tab = %LevelTab
 
 
 func _ready():
 	var level = preload("res://scenes/levels/level1.tscn")
 	var instance = level.instantiate()
 	level_container.set_level(instance)
+
+	level_tab.name = "Level 1"
