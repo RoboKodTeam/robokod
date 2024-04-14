cf5ba78787b4929e315327fbe8b654e6082dca6d
MikronT <vlad.ivanov.if@gmail.com>
2024-04-14 16:51:51 +0300

    Create `open_level()` function

--- scenes/ide/ide_test.gd
+++ scenes/ide/ide_test.gd
@@ -5,8 +5,12 @@ extends Control
 
 
 func _ready():
-	var level = preload("res://scenes/levels/level1.tscn")
-	var instance = level.instantiate()
+	var level1_resource = preload("res://scenes/levels/level1.tscn")
+	open_level(level1_resource, "Level 1")
+
+
+func open_level(resource, title):
+	var instance = resource.instantiate()
 	level_container.set_level(instance)
 
-	level_tab.name = "Level 1"
+	level_tab.name = title
