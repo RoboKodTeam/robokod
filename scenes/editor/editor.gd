ad835c22d2a69aad67a73a284d05c4b7e08b5d3c
MikronT <vlad.ivanov.if@gmail.com>
2024-04-15 13:26:58 +0300

    Add simple tab-to-4-spaces script formatter

--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -2,39 +2,71 @@ extends CodeEdit
 
 @onready var idle_timer = $IdleTimer
 
-@export var perform_idle_syntax_check: bool = false
+@export var auto_format_on_idle: bool = true
+@export var validate_syntax_on_idle: bool = true
 
 var _lines_with_errors = []
 
 
 func _input(_event):
-	# Restart idle timer every time any input is given
-	if perform_idle_syntax_check:
+	if editable:
+		# Restart idle timer every time any input is given
 		idle_timer.start()
 
 
 func _on_idle_timer_timeout():
-	_parse_code_for_errors()
+	if auto_format_on_idle:
+		_format_code()
+
+	if validate_syntax_on_idle:
+		_validate_syntax()
 
 
-func _parse_code_for_errors():
-	clear_errors()
+func _format_code():
+	if text.count("\t") > 0:
+		# Save scroll progress
+		var scroll_progress = Vector2i(scroll_horizontal, scroll_vertical)
+		# Save caret position
+		var caret_pos = Vector2i(get_caret_column(), get_caret_line())
+		var caret_line = get_line(caret_pos.y)
+
+		if caret_pos.x > 0:
+			# Count tabs prior to caret
+			var tabs_count = caret_line.count("\t", 0, caret_pos.x)
+			if tabs_count > 0:
+				# Update caret coords to leave position unchanged after formatting
+				caret_pos.x += tabs_count * (4 - 1)
+
+		# Replace tabs with spaces
+		text = text.replace("\t", "    ")
+
+		Log.log("Updating caret position to", caret_pos)
+		set_caret_line(caret_pos.y)
+		set_caret_column(caret_pos.x)
+
+		# Restore scroll progress
+		scroll_vertical = scroll_progress.y
+		scroll_horizontal = scroll_progress.x
+
+
+func _validate_syntax():
+	_clear_errors()
 
 	var parser = ScriptParser.new()
 	var notices = parser.parse(text)
 
 	for notice in notices:
-		mark_line_as_flawed(notice.statement.line_number, notice.message)
+		_mark_line_as_flawed(notice.statement.line_number, notice.message)
 
 
-func clear_errors():
+func _clear_errors():
 	for line_number in _lines_with_errors:
 		set_line_background_color(line_number, Color(0, 0, 0, 0))
 
 	_lines_with_errors.clear()
 
 
-func mark_line_as_flawed(line_number: int, message: String):
+func _mark_line_as_flawed(line_number: int, message: String):
 	set_line_background_color(line_number, Color("603c3d"))
 
 	# Show hint if caret at the flawed line
