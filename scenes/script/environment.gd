4d7caeadd1c56cfc8b9c383fa9b882e9a6d931a5
MikronT <vlad.ivanov.if@gmail.com>
2024-04-18 02:38:26 +0300

    Create ScriptEnvironment

---
+++ scenes/script/environment.gd
@@ -0,0 +1,16 @@
+class_name ScriptEnvironment
+
+var _entities: Dictionary = {}
+
+
+func put_entity(names: Array, entity: Node):
+	for name in names:
+		_entities[name] = entity
+
+
+func get_entity(entity_name) -> Node:
+	return _entities.get(entity_name)
+
+
+func reset():
+	_entities.clear()
