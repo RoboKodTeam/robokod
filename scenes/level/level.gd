06969651c6176a6d6c261738b26235225767d652
MikronT <vlad.ivanov.if@gmail.com>
2024-04-21 00:47:17 +0300

    Merge tilemap and level scripts

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -2,15 +2,15 @@ class_name Level
 extends Node2D
 
 @onready var tile_map = $TileMap
-@onready var player_container = $PlayerContainer
+@onready var player_controller = $PlayerController
 
 
 func get_level_dimensions() -> Vector2:
-	var map_dimens = tile_map.get_map_dimensions()
+	var map_rect = tile_map.get_used_rect().size
+	var tile_size = tile_map.tile_set.tile_size
+	var map_scale = tile_map.scale
+
+	var tile_dimens = Vector2(map_rect.x * tile_size.x, map_rect.y * tile_size.y)
+	var map_dimens = Vector2(tile_dimens.x * map_scale.x, tile_dimens.y * map_scale.y)
 	var level_dimens = Vector2(map_dimens.x * scale.x, map_dimens.y * scale.y)
 	return level_dimens
-
-
-# Must return player implementation
-func get_player() -> Player:
-	return player_container.get_child(0) as Player
