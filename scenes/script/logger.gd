e44620876af71ff81cc95fdf52d73eafff13c8a9
MikronT <vlad.ivanov.if@gmail.com>
2024-06-08 10:00:33 +0300

    Add varargs to logger

--- scenes/script/logger.gd
+++ scenes/script/logger.gd
@@ -4,19 +4,63 @@ const _LEVEL_INFO = 1
 const _LEVEL_WARN = 2
 const _LEVEL_ERROR = 3
 
+const _PLACEHOLDER = "4417"
+
 var out_node = null
 
 
-func info(message: String):
+func info(
+	msg1: String,
+	msg2 = _PLACEHOLDER,
+	msg3 = _PLACEHOLDER,
+	msg4 = _PLACEHOLDER,
+	msg5 = _PLACEHOLDER,
+	msg6 = _PLACEHOLDER,
+	msg7 = _PLACEHOLDER
+):
+	var message = _filter_and_merge([msg1, msg2, msg3, msg4, msg5, msg6, msg7])
 	_append(_LEVEL_INFO, message)
 
 
-func warn(message: String):
+func warn(
+	msg1: String,
+	msg2 = _PLACEHOLDER,
+	msg3 = _PLACEHOLDER,
+	msg4 = _PLACEHOLDER,
+	msg5 = _PLACEHOLDER,
+	msg6 = _PLACEHOLDER,
+	msg7 = _PLACEHOLDER
+):
+	var message = _filter_and_merge([msg1, msg2, msg3, msg4, msg5, msg6, msg7])
 	_append(_LEVEL_WARN, message)
 
 
-func error(message: String):
+func error(
+	msg1: String,
+	msg2 = _PLACEHOLDER,
+	msg3 = _PLACEHOLDER,
+	msg4 = _PLACEHOLDER,
+	msg5 = _PLACEHOLDER,
+	msg6 = _PLACEHOLDER,
+	msg7 = _PLACEHOLDER
+):
+	var message = _filter_and_merge([msg1, msg2, msg3, msg4, msg5, msg6, msg7])
 	_append(_LEVEL_ERROR, message)
+
+
+func _filter_and_merge(messages) -> String:
+	var combined = null
+
+	for m in messages:
+		if _PLACEHOLDER == m:
+			break
+
+		if not combined:
+			combined = str(m)
+		else:
+			combined += " " + str(m)
+
+	return combined
 
 
 func _append(level: int, message: String):
