ed2d844a8ed65baa84df96ca43bcb0e77285bd3a
MikronT <vlad.ivanov.if@gmail.com>
2024-06-27 14:39:41 +0300

    Move TileMap related funcs to the class script

--- scenes/constructor/constructor.gd
+++ scenes/constructor/constructor.gd
@@ -10,7 +10,6 @@ extends Control
 
 @onready var _level = $Level
 @onready var _tilemap: LevelTileMap = _level.tilemap
-@onready var _tileset: TileSet = _tilemap.tile_set
 
 var _cursor_element: ElementContainer = null
 
@@ -35,10 +34,10 @@ func _on_library_element_selected(element: ElementContainer):
 
 func _on_window_content_gui_input(event):
 	if event is InputEvent and event.is_action_pressed("mb_left"):
-		_on_try_place_tile()
+		_on_set_cell_smart()
 
 
-func _on_try_place_tile():
+func _on_set_cell_smart():
 	var cell_coords = _get_mouse_cell_coords()
 
 	if _cursor_element:
@@ -48,57 +47,24 @@ func _on_try_place_tile():
 
 		if LevelTileMap.LAYER_WALLS_TOP == layer_name:
 			var walls_vert_element = _library.get_random_walls_vert_element()
-			var tile_pos_below = cell_coords + Vector2(0, 1)
+			var tile_pos_below = cell_coords + Vector2i(0, 1)
 			_place_element(walls_vert_element, tile_pos_below)
 	else:
-		_erase_elements(cell_coords)
+		_tilemap.erase_cell_at_all_layers(cell_coords)
 
 
-func _get_mouse_cell_coords() -> Vector2:
+func _get_mouse_cell_coords() -> Vector2i:
 	var level_scale = _level.scale
 	var mouse_at = get_local_mouse_position() / level_scale
-	var cell_coords: Vector2 = _tilemap.local_to_map(mouse_at)
+	var cell_coords: Vector2i = _tilemap.local_to_map(mouse_at)
 	Log.log("Mouse click", mouse_at, "converted to", cell_coords)
 	return cell_coords
 
 
-func _place_element(element: ElementContainer, cell_coords: Vector2):
+func _place_element(element: ElementContainer, cell_coords: Vector2i):
 	var layer_name = element.layer
-	var layer_id = _get_layer_id(layer_name)
 	var atlas_coords = element.atlas_coords
-	var source_id = _get_tile_set_source_id()
-
-	Log.log("Placing element")
-	Log.log("  - layer_id:    ", layer_id)
-	Log.log("  - cell_coords: ", cell_coords)
-	Log.log("  - source_id:   ", source_id)
-	Log.log("  - atlas_coords:", atlas_coords)
-
-	_tilemap.set_cell(layer_id, cell_coords, source_id, atlas_coords)
-
-
-func _erase_elements(cell_coords: Vector2):
-	for layer_i in _tilemap.get_layers_count():
-		_tilemap.erase_cell(layer_i, cell_coords)
-
-
-func _get_layer_id(layer_name: String) -> int:
-	for layer_i in _tilemap.get_layers_count():
-		var compared_name = _tilemap.get_layer_name(layer_i)
-
-		if layer_name == compared_name:
-			return layer_i
-
-	Log.error("Layer id by name", layer_name, "not found")
-	return -1
-
-
-func _get_tile_set_source_id() -> int:
-	for source_i in _tileset.get_source_count():
-		return _tileset.get_source_id(source_i)
-
-	Log.error("Unable to find tile set source id")
-	return -1
+	_tilemap.set_cell_from_default_atlas(layer_name, cell_coords, atlas_coords)
 
 
 func _on_run_button_pressed():
--- scenes/level/tilemap.gd
+++ scenes/level/tilemap.gd
@@ -5,3 +5,28 @@ const LAYER_WALLS_TOP = "walls_top"
 const LAYER_WALLS_VERT = "walls_vert"
 const LAYER_FLOOR = "floor"
 
+@onready var TILESET_SOURCE_ID = tile_set.get_source_id(0)
+
+
+func set_cell_from_default_atlas(layer_name: String, cell_coords: Vector2i, atlas_coords: Vector2i):
+	var layer_id = get_layer_id(layer_name)
+
+	Log.log("Setting atlas cell", atlas_coords, "on layer", layer_id, "at", cell_coords)
+	set_cell(layer_id, cell_coords, TILESET_SOURCE_ID, atlas_coords)
+
+
+func erase_cell_at_all_layers(cell_coords: Vector2i):
+	for layer_i in get_layers_count():
+		erase_cell(layer_i, cell_coords)
+
+
+func get_layer_id(layer_name: String) -> int:
+	for layer_i in get_layers_count():
+		var compared_name = get_layer_name(layer_i)
+
+		if layer_name == compared_name:
+			return layer_i
+
+	Log.error("Layer id by name", layer_name, "not found")
+	return -1
+
