49592f2d2a50c5cb69279e892ba62a3934a8d338
MikronT <vlad.ivanov.if@gmail.com>
2024-06-28 12:14:48 +0300

    Bind target cell detection to the emulator

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -25,3 +25,10 @@ func _scale(vector: Vector2) -> Vector2:
 	var map_dimens = Vector2(tile_dimens.x * map_scale.x, tile_dimens.y * map_scale.y)
 	var level_dimens = Vector2(map_dimens.x * level_scale.x, map_dimens.y * level_scale.y)
 	return level_dimens
+
+
+func has_player_reached_target_cell() -> bool:
+	var player_pos = player.position_grid
+	var has_reached = tilemap.is_target_cell(player_pos)
+	Log.info("Player", "has" if has_reached else "hasn't", "reached target cell")
+	return has_reached
